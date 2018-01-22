faaa6d3b87d755c8498a2d30cf5f5cf9149cbcb9
AegirAexx <aegir.tomasson@gmail.com>
2018-01-22 20:29:23 +0000

    Added files for queue and stack

---
+++ 02/queue.h
@@ -0,0 +1,18 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+class Queue {
+  public:
+    Queue();
+    virtual ~Queue();
+  
+  private:
+    
+
+
+};
+
+
+
+
+#endif // QUEUE_H
---
+++ 02/stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define STACK_H
+
+class Stack {
+  public:
+    Stack();
+    virtual ~Stack();
+  
+  private:
+    
+
+
+};
+
+
+
+
+#endif // STACK_H
