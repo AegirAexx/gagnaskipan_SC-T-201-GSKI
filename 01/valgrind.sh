8b6fc4f3a83e27f4c40ba3ff3761168dc137fb73
AegirAexx <aegir.tomasson@gmail.com>
2018-05-03 02:28:02 +0000

    Syncing folders

--- 01/valgrind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-valgrind --leak-check=full --track-origins=yes ./out 2> output_valgrind.txt
-valgrind --leak-check=full --track-origins=yes ./out 1> output_valgrind.txt
-
-###
-# valgrind --leak-check=full --track-origins=yes ./char 
-# valgrind --leak-check=full --track-origins=yes ./sq 2> output_txt.valgrind
